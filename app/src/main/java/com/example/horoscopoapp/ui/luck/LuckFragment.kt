6a9a3a1a979957e3c9c588481148c5c8e426744d
sergio97re <sergiodev.app@gmail.com>
2024-05-07 16:15:25 +0200

    add card small image

--- app/src/main/java/com/example/horoscopoapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/example/horoscopoapp/ui/luck/LuckFragment.kt
@@ -1,18 +1,51 @@
 package com.example.horoscopoapp.ui.luck
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
 import androidx.fragment.app.Fragment
 import com.example.horoscopoapp.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.random.Random as Random
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
 
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.ivRulette.setOnClickListener { spinRoulette() }
+    }
+
+    private fun spinRoulette() {
+
+        val random = java.util.Random()
+        val degrees = random.nextInt(1400) + 360
+
+        val animator = ObjectAnimator.ofFloat(binding.ivRulette, View.ROTATION, 0f, degrees.toFloat())
+        animator.duration = 2000
+        animator.interpolator = DecelerateInterpolator()
+        animator.doOnEnd {  }
+        animator.start()
+    }
+
+    private fun slideCard(){
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
