ba2c16398f2a15f597222ea303f24056ee803270
sergio97re <sergiodev.app@gmail.com>
2024-05-13 17:00:42 +0200

    ui testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "com.example.horoscopoapp.CustomTestRunner"
     }
 
     buildTypes {
@@ -59,6 +59,7 @@ android {
 dependencies {
     testImplementation("junit:junit:4.12")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    androidTestImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     val navVersion = "2.7.7"
     val cameraVersion = "1.2.3"
 
@@ -94,6 +95,13 @@ dependencies {
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
     testImplementation("io.mockk:mockk:1.12.3")
 
+    //UiTesting
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
+    androidTestImplementation("com.google.dagger:hilt-android-testing:2.48")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
+    androidTestImplementation("androidx.fragment:fragment-testing:1.7.0")
+    kaptAndroidTest("com.google.dagger:hilt-android-compiler:2.48")
+
 }
